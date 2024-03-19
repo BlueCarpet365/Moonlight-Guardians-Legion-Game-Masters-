d91d04caf038a12ef9ef49996ab2b99f26b82170
valentinohp <valentinohp11@gmail.com>
2024-03-19 23:15:41 +0700

    Added animation and build limit

--- Assets/Scripts/CharaControl.cs
+++ Assets/Scripts/CharaControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharaControl : MonoBehaviour
 {
     public float speed = 5f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject playerModel;
 
     void Update()
     {
@@ -13,5 +15,14 @@ public class CharaControl : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
         transform.Translate(movement);
+        if (movement != Vector3.zero)
+        {
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, Quaternion.LookRotation(movement), Time.deltaTime * 40f);
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 }
